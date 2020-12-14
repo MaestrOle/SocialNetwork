e7de0b230ddb98811c24893e72b2d290472c929c
Oleg Kravtcov <71754272+MaestrOle@users.noreply.github.com>
2020-12-14 14:55:53 +0200

    Add files via upload

---
+++ DTO.Cassandra/CUserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cassandra;
+
+namespace DTO.Cassandra
+{
+    public class CUserDTO
+    {
+        public string UserName { get; set; }
+        public TimeUuid LastUpdatedAt { get; set; }
+        public Guid PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string Content { get; set; }
+
+        private List<CommentsDTO> Comments { get; set; }
+    }
+}
---
+++ DTO.Cassandra/CommentsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cassandra;
+
+namespace DTO.Cassandra
+{
+    public class CommentsDTO
+    {
+        public TimeUuid CommentId { get; set; }
+        public string Comment { get; set; }
+        public Guid Commentator { get; set; }
+    }
+}
