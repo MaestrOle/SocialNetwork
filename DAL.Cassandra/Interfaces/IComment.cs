0a7ef7dfe10a689d4319c2fdc16be421887bc486
Oleg Kravtcov <71754272+MaestrOle@users.noreply.github.com>
2020-12-14 14:56:17 +0200

    Add files via upload

---
+++ DAL.Cassandra/Interfaces/IComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cassandra;
+
+namespace DAL.Cassandra.Interfaces
+{
+    public interface IComment
+    {
+        void AddComment(ISession session);
+    }
+}
---
+++ DAL.Cassandra/Interfaces/IUserStream.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cassandra;
+using DTO.Cassandra;
+
+namespace DAL.Cassandra.Interfaces
+{
+    public interface IUserStream
+    {
+       void SyncUserStream(ISession session, Guid postId, TimeUuid updatedAtPrev);
+    }
+}
